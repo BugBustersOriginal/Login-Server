f679c267ab7a0e3670d7cc839f82347e8df48bec
debrazhang <debrazhang09@gmail.com>
2023-03-16 01:20:47 -0700

    for integration test commit out https wareframe

--- server/controller/index.js
+++ server/controller/index.js
@@ -4,21 +4,29 @@ const getSignUp = (req, res) => {
   //render signup page
   res.send('should render signup page');
 };
+
+
+/*****  by username and password to sign up *******/
 const postSignUp = async (req, res) => {
+
+  // check request has both username and password
+  if (req.body.username === undefined || req.body.password === undefined) {
+    console.log('should input both username and password');
+    res.send('redirect to signup page');
+  }
   const {username, password} = req.body;
-  console.log('see if cookie and session exist', req.cookie, req.session)
+
   try {
     let findUser = await getUser({username});
     if (findUser === null) {
       //new user
      let addUser = await createUser({username, password});
-     //connect session
 
-     //render app inner page
-     res.send('signup success, should render app inner page');
+     //render login page
+     res.send('signup success, should render login page');
     } else {
      //user exist, render signup page
-     res.send('alert user that username exist,if want to login please move to login page, or get a new username, rerender signup page');
+     res.send('alert user that username exist, if want to login please move to login page, or get a new username, rerender signup page');
     }
   } catch (err) {
     console.log('signup error', err);
@@ -28,10 +36,22 @@ const getLogIn = (req, res) => {
   res.send('should render login page')
 
 };
+
 const postLogIn = (req, res) => {
 
 
 };
+
+const postAddress = (req, res) => {
+
+};
+const postAvator = (req, res) => {
+
+};
+const forgetOrChangePassword = (req, res) => {
+
+};
+
 const getLogOut = (req, res) => {
 
 };
--- server/index.js
+++ server/index.js
@@ -1,6 +1,24 @@
 require('dotenv').config();
+/******** for https request */
+// const https = require('https');
+// const fs = require('fs');
+// const path = require('path');
+
 const app = require('./app.js');
 
+
+/*****  Load SSL/TLS certificate and key ****/
+// const options = {
+//   key: fs.readFileSync(path.resolve(__dirname, '../key.pem')),
+//   cert: fs.readFileSync(path.resolve(__dirname,'../cert.pem'))
+// };
+/******  Create HTTPS server *****/
+// const server = https.createServer(options, app);
+
+// server.listen(process.env.port, () => {
+//   console.log(`Login-Server is listening on ${process.env.port}`);
+// });
+
 app.listen(process.env.port, () => {
-  console.log(`Login-Server is listening on ${process.env.port}`);
-});
+    console.log(`Login-Server is listening on ${process.env.port}`);
+  });
